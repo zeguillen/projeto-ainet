b15eb05ca74feeb7886d4156536efce866cf4d34
Luis Pires <luismpires1997@hotmail.com>
2019-05-16 22:18:16 +0100

    some

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,4 +36,9 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    protected function credentials(\Illuminate\Http\Request $request)
+    {
+        return ['email' => $request->{$this->username()}, 'password' => $request->password, 'ativo' => 1];
+    }
 }
