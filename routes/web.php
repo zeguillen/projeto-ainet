8f0805588c6bcf673cffddb1759318e7922dbaf7
Luis Pires <luismpires1997@hotmail.com>
2019-05-22 23:19:31 +0100

    Aeronaves list

--- routes/web.php
+++ routes/web.php
@@ -30,14 +30,11 @@ Route::post('/socios/create', 'UserController@store')->name('users.store');
 Route::get('/socios/{id}/edit', 'UserController@edit')->name('users.edit');
 Route::put('/socios/{id}/edit', 'UserController@update')->name('users.update');
 Route::delete('/socios/{id}', 'UserController@destroy')->name('users.destroy');
-
-//26
 Route::patch('/socios/{id}/quota', 'UserController@changeQuota')->name('quota.change');
 Route::patch('/socios/reset_quotas', 'UserController@resetQuotas')->name('quotas.reset');
-
-//27
 Route::patch('/socios/desativar_sem_quotas', 'UserController@desativarUsersSemQuotas')->name('users.desativar');
 Route::patch('/socios/{id}/ativo', 'UserController@changeAtivo')->name('ativo.change');
+
 //aeronaves 
 Route::get('aeronaves', 'AeronaveController@index')->name('aeronaves.index');
 Route::get('/aeronaves/create', 'AeronaveController@create')->name('aeronaves.create');
