c7b7cb95df2f3dbc948341709a1ef988d35d4a5b
Luis Pires <luismpires1997@hotmail.com>
2019-05-29 20:36:54 +0100

    False -> true

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,6 +9,7 @@ use Auth;
 use Illuminate\Http\Request;
 use Illuminate\Validation\Rule;
 use Illuminate\Support\Facades\Hash;
+use App\Http\Requests\UserStorageRequest;
 
 use Illuminate\Support\Facades\DB;
 
--- app/Http/Requests/UserStorageRequest.php
+++ app/Http/Requests/UserStorageRequest.php
@@ -13,7 +13,7 @@ class UserStorageRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
