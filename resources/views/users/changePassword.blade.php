5a03788d41dbc0bff3f5b05380eb5f80b89be0d6
Luis Pires <luismpires1997@hotmail.com>
2019-06-01 18:37:45 +0100

    Tirei o erro a mais

--- resources/views/users/changePassword.blade.php
+++ resources/views/users/changePassword.blade.php
@@ -8,27 +8,21 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Change Password') }}</div>
+                <div class="card-header">Change Password</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.save') }}">
                         @csrf
                         @method('PATCH')
                         <div class="form-group row">
-                            <label for="" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+                            <label for="old_password" class="col-md-4 col-form-label text-md-right">Password</label>
 
                             <div class="col-md-6">
-                                <input id="old_password'" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="old_password'" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                                <input id="old_password" type="password" class="form-control" name="old_password" required>
                             </div>
                         </div>
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('New Password') }}</label>
+                            <label for="password" class="col-md-4 col-form-label text-md-right">New Password</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -36,7 +30,7 @@
                         </div>
 
                         <div class="form-group row">
-                            <label for="password_confirmation" class="col-md-4 col-form-label text-md-right">{{ __('Confirm Password') }}</label>
+                            <label for="password_confirmation" class="col-md-4 col-form-label text-md-right">Confirm Password</label>
 
                             <div class="col-md-6">
                                 <input id="password_confirmation" type="password" class="form-control" name="password_confirmation" required>
@@ -46,7 +40,7 @@
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ ('Change Password') }}
+                                    Change Password
                                 </button>
                             </div>
                         </div>
